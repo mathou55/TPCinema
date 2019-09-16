99fad0c1ea1441876d8f2c255b9d71d4a3842ccf
PFISTER Mathias <matpfi@outlook.fr>
2019-09-16 10:26:51 +0200

    vendrePlaces()

--- Cinema/Salle.cs
+++ Cinema/Salle.cs
@@ -8,50 +8,52 @@ namespace Cinema
 {
     class Salle
     {
+        private int numeroSalle;
         private string titre;
         private int nmbPlace;
         private double prixUnitaire;
         private int nmbPlaceTarifN = 0;
         private int nmbPlaceTarifR = 0;
 
-        // Constructeur
+        // Constructeur de Salle
         public Salle(string unTitre, int unNmbPLace, double unPrixUnitaire)
         {
-            this.titre = unTitre;
-            this.nmbPlace = unNmbPLace;
-            this.prixUnitaire = unPrixUnitaire;
+            this.Titre = unTitre;
+            this.NmbPlace = unNmbPLace;
+            this.PrixUnitaire = unPrixUnitaire;
         }
 
-        public string getTitre()
-        {
-            return titre;
-        }
-
-        public int getNmbPlace()
-        {
-            return nmbPlace;
-        }
-
-        public double getPrixUnitaire()
-        {
-            return prixUnitaire;
-        }
-
-        public int getNmbPlaceTarifN()
-        {
-            return nmbPlaceTarifN;
-        }
-
-        public int getNmbPlaceTarifR()
-        {
-            return nmbPlaceTarifR;
-        }
+        public string Titre { get => titre; set => titre = value; }
+        public int NmbPlace { get => nmbPlace; set => nmbPlace = value; }
+        public double PrixUnitaire { get => prixUnitaire; set => prixUnitaire = value; }
+        public int NmbPlaceTarifN { get => nmbPlaceTarifN; set => nmbPlaceTarifN = value; }
+        public int NmbPlaceTarifR { get => nmbPlaceTarifR; set => nmbPlaceTarifR = value; }
+        public int NumeroSalle { get => numeroSalle; set => numeroSalle = value; }
 
         public int nbPlacesDisponibles()
         {
-            int placeDispo = nmbPlace - (nmbPlaceTarifN + nmbPlaceTarifR);
+            int placeDispo = NmbPlace - (NmbPlaceTarifN + NmbPlaceTarifR);
             return placeDispo;
         }
 
+        public void vendrePlaces(int nbre, bool tarifReduit)
+        {
+            if (nbre > nmbPlace)
+            {
+                return "La vente n'est pas disponible";
+            }
+            else
+            {
+                if (tarifReduit == true)
+                {
+                    prixUnitaire = (prixUnitaire * 0.20);
+                    return "Le prix est de" + prixUnitaire + " €";
+                }
+                else
+                {
+                    return "Le prix est de" + prixUnitaire + " €";
+                }
+            }
+        }
     }
 }
